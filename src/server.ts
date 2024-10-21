e4164ef2dc5a149e87e5228f258d82da94041ccf
D1spy <159242794+D1spi@users.noreply.github.com>
2024-10-21 23:20:59 +0200

    k

---
+++ src/server.ts
@@ -0,0 +1,38 @@
+import './loadEnvironment';
+import express from 'express';
+
+// Initialize expres
+const app = express();
+const port = process.env.HOST_PORT ?? '3000';
+
+// Lista de estudiantes
+const estudiantes: string[] = [
+  "Santiago Fernández-Pedrera",
+  "Nahomy Argueta",
+  "Daniel Galeano",
+  "Denis Podovei",
+  "Ivan Vallejo",
+  "Nil Marquez",
+  "Paul Aznar Tequén",
+  "Òscar Cruañas",
+  "Quim Pallarés",
+  "Satenik Avetisyan",
+  "Pol Cubo",
+  "Oriol Fontcuberta Aduart"
+];
+
+// Ruta para obtener la lista de estudiantes
+app.get('/a', (req, res) => res.json({ estudiantes }));
+
+// Ruta para contar el número de estudiantes
+app.get('/count', (req, res) => {
+  const count = estudiantes.length; // Contar el número de estudiantes
+  res.json({ count });
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+export default app;
